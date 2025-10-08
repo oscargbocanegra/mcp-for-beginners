877a5fe2005666418177b298c38766062347e7b5
Oscar Giovanni <oscargiovanni@gmail.com>
2025-10-07 21:50:18 -0500

    Actualizar herramientas de cálculo en el servidor MCP: agregar funciones de suma, resta, multiplicación y división con esquemas de entrada adecuados.

--- mcp_server_ts/main.js
+++ mcp_server_ts/main.js
@@ -1,25 +1,50 @@
 import { McpServer, ResourceTemplate } from '@modelcontextprotocol/sdk/server/mcp.js';
 import { StdioServerTransport } from '@modelcontextprotocol/sdk/server/stdio.js';
 import { z } from 'zod';
+//creacion del server
 const server = new McpServer({
-    name: "server-mcp-ts",
+    name: "server-mcp-calculadora-ts",
     version: "0.0.1"
+});
+//registro de herramientas y recursos
+server.registerTool("sumar", {
+    title: "Sumar",
+    description: "Suma dos números",
+    inputSchema: { n1: z.number(), n2: z.number() }
+}, async ({ n1, n2 }) => {
+    let operation = String(n1 + n2);
+    return {
+        content: [{ type: "text", text: operation }]
+    };
+});
+server.registerTool("restar", {
+    title: "Restar",
+    description: "Resta dos números",
+    inputSchema: { n1: z.number(), n2: z.number() }
+}, async ({ n1, n2 }) => {
+    let operation = String(n1 - n2);
+    return {
+        content: [{ type: "text", text: operation }]
+    };
 });
 server.registerTool("multiplicar", {
     title: "Multiplicar",
-    description: "Multiplica dos números",
-    inputSchema: { number1: z.number(), number2: z.number() }
-}, async ({ number1, number2 }) => {
-    if (typeof number1 !== "number" || typeof number2 !== "number") {
-        throw new Error("Los parámetros deben ser números");
-    }
+    description: "Multiplicar dos números",
+    inputSchema: { n1: z.number(), n2: z.number() }
+}, async ({ n1, n2 }) => {
+    let operation = String(n1 * n2);
     return {
-        content: [
-            {
-                type: "text",
-                text: String(number1 * number2)
-            }
-        ]
+        content: [{ type: "text", text: operation }]
+    };
+});
+server.registerTool("dividir", {
+    title: "Dividir",
+    description: "Dividir dos números",
+    inputSchema: { n1: z.number(), n2: z.number() }
+}, async ({ n1, n2 }) => {
+    let operation = String(n1 % n2);
+    return {
+        content: [{ type: "text", text: operation }]
     };
 });
 server.registerResource("saludar", new ResourceTemplate("saludar://{nombre}", { list: undefined }), { title: "Saludar", description: "Saluda a una persona por su nombre" }, async (url, { nombre }) => {
